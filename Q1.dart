5c5e6cb8ecf219eb948349d4669384c532991633
Arifk11965 <arif.k11965@gmail.com>
2024-12-05 21:44:58 +0500

    Directory Sync

---
+++ Q1.dart
@@ -0,0 +1,14 @@
+// 1.Create two integer variables length and breadth and assign values then check if they
+// are square values or rectangle values. ie: if both values are equal then it's square
+// otherwise rectangle.
+//
+
+void main() {
+  int length = 25;
+  int breadth = 25;
+  if (length == breadth) {
+    print("Selected Ranges Represent The shape is = square.");
+  } else {
+    print("Selected Ranges Represent The shape is = rectangle.");
+  }
+}
