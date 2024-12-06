0adfe937b6325f43a17e65cddc0b1062e5e21880
Arifk11965 <arif.k11965@gmail.com>
2024-12-06 23:20:46 +0500

    'update'

--- Q8.dart
+++ Q8.dart
@@ -1,11 +1,11 @@
-// 8. Check if the number is even or odd? i.e : Even numbers are completely divisible by
-// 2. (2,4,6,8,10,….
+//Check if the number is even or odd? i.e : Even numbers are completely divisible by 2. //(2,4,6,8,10,….
 
 import 'dart:io';
 
 void main() {
   print("Hi, please choose a number: ");
   int number = int.parse(stdin.readLineSync()!);
+
   if (number % 2 == 0) {
     print("Chosen number is even");
   } else {
