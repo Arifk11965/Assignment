120349edff3c7ce881e20d19637277dc677fa7fe
Arifk11965 <arif.k11965@gmail.com>
2024-12-06 11:09:21 +0500

    'update'

---
+++ Q6.dart
@@ -0,0 +1,16 @@
+// 6. Write a program to check whether an alphabet is a vowel or consonant.
+// 
+if (input == "a" ||
+input == "e" ||
+input == "i" ||
+input == "o" ||
+input == "u") {
+print("you entered vowel");
+} else if (input != "a" ||
+input != "e" ||
+input != "i" ||
+input != "o" ||
+input != "u") {
+print("you entered Consonant");
+}
+}
