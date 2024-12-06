885ab3517923f634159c67c834ce4a398cb078e9
Arifk11965 <arif.k11965@gmail.com>
2024-12-06 23:35:15 +0500

    'update'

--- Q9.dart
+++ Q9.dart
@@ -1,11 +1,11 @@
-// 9. Check if a number is even then check if its divisible by 5 or not & if a number is odd
-// then check if its divisible by 7 or not.
+//Check if a number is even then check if its divisible by 5 or not & if a number is odd then //check if its divisible by 7 or not.
 
 import 'dart:io';
 
 void main() {
   print("Hi, please choose a number: ");
   int number = int.parse(stdin.readLineSync()!);
+
   if (number % 2 == 0) {
     if (number % 5 == 0) {
       print('$number is even and divisible by 5.');
