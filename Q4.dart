b7c8c13322f8053baa0cb48b401ef53af56abc42
Arifk11965 <arif.k11965@gmail.com>
2024-12-06 10:03:10 +0500

    'modify'

---
+++ Q4.dart
@@ -0,0 +1,14 @@
+//4. Write a program to convert Celsius to Fahrenheit .i.e: Temperature in degrees
+// Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
+
+import 'dart:io';
+void main () {
+  int temp = 40;
+  print("Enter temperature in Celsius:");
+  var input = stdin.readLineSync()!;
+  double celsius = double.parse(input);
+  double fahrenheit = (celsius * 9 / 5) + 32;
+  {
+    print("$celsius C is equal to $fahrenheit°F");
+  }
+}
