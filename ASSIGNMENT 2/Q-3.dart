b6ff02205d611bfa724e180e16951e499ccd04c9
Arifk11965 <arif.k11965@gmail.com>
2024-12-12 00:43:44 +0500

    'update'

---
+++ ASSIGNMENT 2/Q-3.dart
@@ -0,0 +1,12 @@
+//  Q.3: 	Create a list of Days and remove one by one from the end of list.
+void main() {
+  List days = [];
+  days.remove("Saturday");
+  days.remove("Sunday");
+  days.remove("Monday");
+  days.remove("Tuesday");
+  days.remove("Wednesday");
+  days.remove("Thursday");
+  days.remove("Friday");
+  print(days);
+}
