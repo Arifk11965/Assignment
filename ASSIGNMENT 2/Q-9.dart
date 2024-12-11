a8b50c760f7a4112e2e59c3e6653b8f716f063c0
Arifk11965 <arif.k11965@gmail.com>
2024-12-12 00:52:07 +0500

    'update'

---
+++ ASSIGNMENT 2/Q-9.dart
@@ -0,0 +1,9 @@
+// Q.9: Given a list of integers, write a dart code that returns the maximum value from the list.
+
+void main() {
+  List<int> numbers = [3, 9, 18, 12, 6, 7];
+
+  numbers.sort();
+  int maxValue = numbers.last;
+  print('Maximum value: $maxValue');
+}
