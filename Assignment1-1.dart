acc1460416fc67d35545d7052e587806b57fab9e
Arifk11965 <arif.k11965@gmail.com>
2024-12-12 20:26:56 +0500

    úpdate

--- Assignment1-1.dart
+++ Assignment1-1.dart
@@ -5,8 +5,7 @@
 
 void main() {
   int length = 50;
- int breadth = 25;
-  
+  int breadth = 25;
   if (length == breadth) {
     print("Selected Ranges Represent The shape is = square.");
   } else {
---
+++ Assignment1-7.dart
@@ -0,0 +1,6 @@
+void main() {
+  List myList = [1, 2, 4, 3, 5, 6, 7];
+  {
+    print(myList.sort);
+  }
+}
